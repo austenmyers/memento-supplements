bb0657cdc79f4e0bfb81cf79234951b62a4e1efd
Austen Myers <austenc.id@pm.me>
2025-08-07 12:43:45 -0600

    calll getid in search

--- get_dsld.js
+++ get_dsld.js
@@ -1,15 +1,5 @@
 var api = 'https://api.ods.od.nih.gov/dsld/v9/'
 
-function searchByBarCode(bar_code){
-    var endpoint = 'search-filter?q='
-    var url = api + endpoint + bar_code
-    var response = http().get(url)
-    if (response.code == 200){
-        return JSON.parse(response.body)
-    }
-    else {return false}
-}
-
 function getID(data){
     var hits = data["hits"]
     var hit = hits[0]
@@ -18,6 +8,20 @@ function getID(data){
     }
     else {return false}
 }
+
+function searchByBarCode(bar_code){
+    var endpoint = 'search-filter?q='
+    var url = api + endpoint + bar_code
+    var response = http().get(url)
+    if (response.code == 200){
+        var data =  JSON.parse(response.body)
+        var id = getID(data)
+        return id
+    }
+    else {return false}
+}
+
+
 
 function getLabelData(dsld_id){
     var endpoint = 'label/'

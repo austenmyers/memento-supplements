763f073965863931d34ec5414312373a8db32e57
Austen Myers <austenc.id@pm.me>
2025-08-07 11:26:40 -0600

    make search url

--- dsld.js
+++ dsld.js
@@ -1,1 +1,6 @@
-
+// Create URL with barcode.
+function makeURL(bar_code){
+    var api = 'https://api.ods.nih.gov/dsld/v9/'
+    var endpoint = 'search-filter?q='
+    return api + endpoint + bar_code
+}

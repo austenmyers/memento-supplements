d9aa6e220e66afd430bae367b4f934a939cabb55
Austen Myers <austenc.id@pm.me>
2025-08-07 12:07:11 -0600

    return parsed data

--- dsld.js
+++ dsld.js
@@ -5,7 +5,8 @@ function searchByBarCode(bar_code){
     var url = api + endpoint + bar_code
     var response = http().get(url)
     if (response.code == 200){
-        return response.body
+        var results = JSON.parse(response.body)
+        return results
     }
     else {
         return false
